e0bceb779f8c14f338d6c8ad1e3f40c56a94f5ba
DevAOJ <olutuyo80@live.com>
2023-10-19 18:13:53 +0100

    modified import component path

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,5 @@
 import {Link} from "react-router-dom";
-import image  from "/Users/user/Desktop/reactFolder/counter-app/src/Components";
+import image  from "/src/Components";
 
 const Home = () => {
   return (
